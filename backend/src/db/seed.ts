c4ae9a89e12dea9471e0b4e39cf5089ef4d1d242
Hugo Ramon <gustavo@gltecnologia.com.br>
2024-09-13 20:36:38 -0300

    Initial config and created first tables and installed dependencies

---
+++ backend/src/db/seed.ts
@@ -0,0 +1,30 @@
+import { client, db } from ".";
+import { goalCompletions, goals } from "./schema";
+import dayjs from "dayjs";
+
+async function seed() {
+  await db.delete(goalCompletions);
+  await db.delete(goals);
+
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: "Wake up early", desiredWeeklyFrequency: 5 },
+      { title: "Exercise", desiredWeeklyFrequency: 4 },
+      { title: "Meditate", desiredWeeklyFrequency: 2 },
+    ])
+    .returning();
+
+  const startOfWeek = dayjs().startOf("week");
+
+  await db.insert(goalCompletions).values([
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.add(1, "day").toDate() },
+  ]);
+}
+
+seed().finally(() => {
+  client.end();
+});
+
+// This seed file is used to populate our database with fictitious data for the table not to be empty
